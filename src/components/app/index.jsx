e0b6a7cd91fef058692f92a4d4344009b2caf28b
AlexKsenych <alex.ksenych04@gmail.com>
2022-09-07 19:58:27 +0300

    added template

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,12 +1,14 @@
 import './app.sass'
 import Header from '../header'
 import Flashcards from '../flashcards'
+import Template from '../template'
 
 const App = () => {
     return (
         <div className='app'>
             <Header />
-            <Flashcards/>
+            <Template/>
+            {/* <Flashcards/> */}
         </div>
     )
 }
---
+++ src/components/template/index.js
@@ -0,0 +1,14 @@
+import './template.sass'
+
+const Template = () => {
+    return (
+        <div className='templates'>
+            <div className='templates__template'>Classic Flashcards</div>
+            <div className='templates__template'>Flashcards on time</div>
+            <div className='templates__template'>Find out a definition</div>
+            <div className='templates__template'>Kahoot-like</div>
+        </div>
+    )
+}
+
+export default Template
