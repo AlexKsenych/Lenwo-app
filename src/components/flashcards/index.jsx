f9ac5bdea9ad3b22d618d1dad71f2f2f5dc052b9
AlexKsenych <alex.ksenych04@gmail.com>
2022-09-10 00:27:18 +0300

    added interactive to flashcards; header link updated

--- src/components/flashcards/index.jsx
+++ src/components/flashcards/index.jsx
@@ -1,49 +1,60 @@
 import './flashcards.sass'
-import arrow from '../../assets/img/arrow.png'
+// import arrow from '../../assets/img/arrow.png'
 import {useSearchParams} from 'react-router-dom'
 import { useState } from 'react'
 
 const Flashcards = ({data}) => {
+    const [currentNum, setCurrentNum] = useState(0)
+    const [unknownWords, setUnknownWords] = useState([])
     const [isActive, setIsActive] = useState(false)
     const [searchParams] = useSearchParams()
-    const searchId = +searchParams.get('id')
+    const searchId = searchParams.get('id')
 
-    const {title, words} = data.find(item => item.id === searchId)
+    const {title,  words} = data.find(item => item.id === searchId)
+    const {id, name, descr} = words[currentNum]
     const img = null
+
+    const activeClass = isActive ? 'active' : ''
 
     const onFlashcardClick = () => setIsActive(!isActive)
 
-    const activeClass = isActive ? 'active' : ''
+    const onNextClick = (e, id) => {
+        e.stopPropagation()
+        if (id) setUnknownWords([...unknownWords, id])
+        setCurrentNum(currentNum + 1)
+    }
+
+    console.log(unknownWords)
 
     return (
         <main className='flashcards'>
             <div className='flashcards__title'>{title}</div>
-            <div className="flashcards__arrow">
-                <img src={arrow} alt="leftArrow" className="flashcards__arrow__left" />
-            </div>
             <div onClick={onFlashcardClick} className='flashcards__flashcard'>
-                <div className="flashcards__flashcard__count">{`${0} / ${words.length}`}</div>
+                <div className="flashcards__flashcard__count">{`${currentNum + 1} / ${words.length}`}</div>
                 <div className={`flashcards__flashcard__inner ${activeClass}`}>
                     <div className="flashcards__flashcard__inner__front">
-                        <div className='flashcards__flashcard__inner__front__word'>puppy</div>
+                        <div className='flashcards__flashcard__inner__front__word'>{name}</div>
                             <div className='flashcards__flashcard__inner__front__btns'>
-                                <button className='flashcards__flashcard__inner__front__btns__btn'>idk</button>
-                                <button className='flashcards__flashcard__inner__front__btns__btn'>Yeah I Knew That</button>
+                                <button onClick={(e) => onNextClick(e , id)} className='flashcards__flashcard__inner__front__btns__btn'>idk</button>
+                                <button onClick={onNextClick} className='flashcards__flashcard__inner__front__btns__btn'>Yeah I Knew That</button>
                             </div>
                         </div>
                     <div className="flashcards__flashcard__inner__back">
                         {img ? <img src={img} alt="wordImage" className="flashcards__flashcard__inner__back__img" /> : null}
-                        <div className="flashcards__flashcard__inner__back__definition">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quasi atque ab?
-                        </div>
+                        <div className="flashcards__flashcard__inner__back__definition">{descr}</div>
                     </div>
                 </div>
-            </div>
-            <div className="flashcards__arrow">
-                <img src={arrow} alt="rightArrow" className="flashcards__arrow__right" />
             </div>
         </main>
     )
 }
 
 export default Flashcards
+
+
+/* <div onClick={() => onChangeNum(false)} className="flashcards__arrow">
+    <img src={arrow} alt="leftArrow" className="flashcards__arrow__left" />
+</div>
+<div onClick={() => onChangeNum(true)} className="flashcards__arrow">
+    <img src={arrow} alt="rightArrow" className="flashcards__arrow__right" />
+</div> */
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,10 @@
 import './header.sass'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <header className='header'>
-            <div className='header__title'>Lenwo</div>
+            <Link to={'/'} className='header__title'>Lenwo</Link>
         </header>
     )
 }
